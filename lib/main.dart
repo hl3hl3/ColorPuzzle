b9db3e782a7f3ceeb467349d67fbd77e543113f0
Rebecca Hsieh <rebecca.hsieh77@gmail.com>
2019-10-26 16:54:16 +0800

    整理程式碼＆版面

--- lib/main.dart
+++ lib/main.dart
@@ -7,42 +7,67 @@ class DragGameApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyGame',
-      home: Scaffold(
-        body: Padding(
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Draggable & DragTarget'),
+      ),
+      body: Center(
+        child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Draggable(
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                ),
-                feedback: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
+                  child: ColorSquare(color: Colors.orange),
+                  feedback: ColorSquare(color: Colors.orange),
+                  data: Colors.orange),
+              SizedBox(height: 20, width: 20),
               Draggable(
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.blue,
-                ),
-                feedback: Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                ),
-              ),
+                  child: ColorSquare(color: Colors.lightGreen),
+                  feedback: ColorSquare(color: Colors.lightGreen),
+                  data: Colors.lightGreen),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+const double squareWidth = 100;
+class ColorSquare extends StatelessWidget {
+  final Color color;
+
+  const ColorSquare({
+    Key key,
+    @required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: squareWidth,
+      height: squareWidth,
+      color: color,
     );
   }
 }
